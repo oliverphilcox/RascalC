7b76293bd9a2001773356114d5685c8b41bf03d9
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2018-09-14 11:56:45 -0400

    modularization

---
+++ modules/integrals.h
@@ -0,0 +1,447 @@
+// integrals class for grid_covariance.cpp (originally from Alex Wiegand)
+
+#ifndef INTEGRALS_H
+#define INTEGRALS_H
+
+class Integrals {
+
+	public:
+//	RandomDraws *rd;
+	CorrelationFunction *cf;
+
+#ifdef ALLOUT
+	Float rij_magt;
+	Float rij_mut;
+	Float rjk_magt;
+	Float rjk_mut;
+#endif
+
+	private:
+	int nbin,mbin;
+	Float rmin,rmax,mumin,mumax,dr,dmu; // Ranges in r and mu
+	Float *Ra, *cx, *c2, *c3, *c4; // Arrays to accumulate the integrals
+	Float *Raerr, *cxerr, *c2err, *c3err, *c4err; // Arrays to accumulate the errors; however, currently used for other purposes!
+	Float xiij; //Only temporary for xi^2
+
+	uint64 *binct,*binct3,*binct4; // Arrays to accumulate the counts of the bins
+	bool box,rad; // Flags to decide whether we have a periodic box and if we have a radial correlation function only
+
+#ifdef ALLOUT
+	FILE**rij_magf;
+	FILE**rij_muf;
+	FILE**rik_magf;
+	FILE**rjk_magf;
+	FILE**rik_muf;
+	FILE**rjk_muf;
+#endif
+
+	double empty[8];   // Leftover from grid_multipoles. Probably no longer needed
+
+	  public:
+		Integrals(Parameters *par, CorrelationFunction *_cf){
+			cf=new CorrelationFunction(_cf);
+			init(par);
+		}
+
+//		void init_cf_rd(double*x,long n,Parameters *par) {
+//			cf=new CorrelationFunction(par->corname,par->mbin,par->mumax-par->mumin);
+//			if(n==0)
+//				rd=new RandomDraws(cf,par,x,n);
+//			else
+//				rd=new RandomDraws(cf,par,x,n);
+//		}
+
+	    void init(Parameters *par) {
+
+	    	nbin=par->nbin;
+	    	mbin=par->mbin;
+
+			int ec=0;
+			// Initialize the binning
+			ec+=posix_memalign((void **) &Ra, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &cx, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &c2, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &c3, PAGE, sizeof(double)*nbin*mbin*nbin*mbin);
+			ec+=posix_memalign((void **) &c4, PAGE, sizeof(double)*nbin*mbin*nbin*mbin);
+
+			ec+=posix_memalign((void **) &Raerr, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &cxerr, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &c2err, PAGE, sizeof(double)*nbin*mbin);
+			ec+=posix_memalign((void **) &c3err, PAGE, sizeof(double)*nbin*mbin*nbin*mbin);
+			ec+=posix_memalign((void **) &c4err, PAGE, sizeof(double)*nbin*mbin*nbin*mbin);
+
+			ec+=posix_memalign((void **) &binct, PAGE, sizeof(uint64)*nbin*mbin);
+			ec+=posix_memalign((void **) &binct3, PAGE, sizeof(uint64)*nbin*mbin*nbin*mbin);
+			ec+=posix_memalign((void **) &binct4, PAGE, sizeof(uint64)*nbin*mbin*nbin*mbin);
+
+			assert(ec==0);
+
+			reset();
+
+			box=par->perbox;
+
+			rmax=par->rmax;
+			rmin=par->rmin;
+
+			mumax=par->mumax;
+			mumin=par->mumin;
+
+			dr=(rmax-rmin)/nbin;
+			dmu=(mumax-mumin)/mbin;
+
+	    	rad=mbin==1&&dmu==1.;
+
+			empty[0] = 0.0;   // To avoid a warning
+
+#ifdef ALLOUT
+			rij_magf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+
+	//		rij_muf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+	//		rik_magf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+	//		rjk_magf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+	//		rik_muf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+	//		rjk_muf=(FILE**)malloc(sizeof(FILE*)*nbin*mbin);
+
+			char buf[2000];
+
+			for (int j=0; j<nbin*mbin; j++) {
+
+				snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_xiright_xisampling/own_10Mpc_all2pt_rij_mag-%d-%d.txt", j/mbin, j%mbin);
+				rij_magf[j]=fopen(buf,"w");
+	//			snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_all2pt_rij_mu-%d-%d.txt", j/mbin, j%mbin);
+	//			rij_muf[j]=fopen(buf,"w");
+	//			snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_all2pt_rik_mag-%d-%d.txt", j/mbin, j%mbin);
+	//			rik_magf[j]=fopen(buf,"w");
+	//			snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_all2pt_rjk_mag-%d-%d.txt", j/mbin, j%mbin);
+	//			rjk_magf[j]=fopen(buf,"w");
+	//			snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_all2pt_rik_mu-%d-%d.txt", j/mbin, j%mbin);
+	//			rik_muf[j]=fopen(buf,"w");
+	//			snprintf(buf, sizeof buf, "/mnt/store1/awiegand/CovarianceCalculation/Data/own_all2pt_rjk_mu-%d-%d.txt", j/mbin, j%mbin);
+	//			rjk_muf[j]=fopen(buf,"w");
+			}
+#endif
+
+		}
+
+		~Integrals() {
+			free(Ra);
+			free(cx);
+			free(c2);
+			free(c3);
+			free(c4);
+			free(c3err);
+			free(c4err);
+			free(binct);
+			free(binct3);
+			free(binct4);
+
+#ifdef ALLOUT
+			for (int j=0; j<nbin*mbin; j++) {
+				fclose(rij_magf[j]);
+//				fclose(rij_muf[j]);
+//				fclose(rik_magf[j]);
+//				fclose(rjk_magf[j]);
+//				fclose(rik_muf[j]);
+//				fclose(rjk_muf[j]);
+			}
+#endif
+//			rd.~RandomDraws();
+	    }
+
+	    void reset(){
+	    	for (int j=0; j<nbin*mbin; j++) {
+				Ra[j] = 0;
+				cx[j] = 0;
+				c2[j] = 0;
+				binct[j] = 0;
+				Raerr[j] = 0;
+				cxerr[j] = 0;
+				c2err[j] = 0;
+			}
+			for (int j=0; j<nbin*mbin*nbin*mbin; j++) {
+				c3[j]=0;
+				c4[j]=0;
+				binct3[j] = 0;
+				binct4[j] = 0;
+				c3err[j]=0;
+				c4err[j]=0;
+			}
+	    }
+
+	    inline int getbin(Float r, Float mu){
+	    	// Linearizes 2D indices
+	       	return floor((r-rmin)/dr) *mbin + floor((mu-mumin)/dmu);
+	    }
+
+	    inline void second(int& bin, const Particle& pi, const Particle& pj, Float pro) {
+	    	// Accumulates the two point integral C2
+	    	// Note that the value of bin is returned
+			Float rij_mag,rij_mu;
+			Float rav,c2v,cxv, weight;
+
+//			Bin will be reused later
+	        cleanup_l(pi.pos, pj.pos, rij_mag, rij_mu);
+	        bin=getbin(rij_mag,rij_mu);
+
+	        if ((bin>=0) && (bin<nbin*mbin)){
+	        	weight=pi.w*pj.w;
+		        xiij=cf->xi(rij_mag,rij_mu);
+	        	rav=weight / pro;
+	        	c2v= weight*weight *(1+xiij) / pro;
+	        	cxv = weight*xiij / pro;
+
+	        	Ra[bin]+= rav;
+	        	c2[bin]+= c2v;
+	        	cx[bin]+= cxv;
+	        	binct[bin]++;
+	        	Raerr[bin]+= pow(rav,2);
+//	        	c2err[bin]+= pow(c2v,2);
+	        	c2err[bin]+= weight*weight / pro * pow(xiij,2); //Misuse to accumulate xi^2 term
+	        	cxerr[bin]+= pow(cxv,2);
+	        }else{
+	        	bin=-1;
+	        }
+#ifdef ALLOUT
+	        rij_magt=rij_mag;
+	        rij_mut=rij_mu;
+#endif
+	    }
+
+	    inline void third(const int& bin, Float& xijk, const Particle& pi, const Particle& pj, const Particle& pk, Float p3) {
+	    	// Accumulates the three-point integral C3
+	    	// Returns the correlation function between point j and k
+	    	int binik;
+	    	Float c3l;
+			Float rik_mag,rik_mu, rjk_mag,rjk_mu, xiik;
+
+			cleanup_l(pi.pos, pk.pos, rik_mag, rik_mu);
+			binik=getbin(rik_mag, rik_mu);
+			xiik=cf->xi(rik_mag, rik_mu); //Only temporary for xi^2; can be commented out if c3err is used for error as intended
+
+//			Correlation function will be reused in fourth
+			cleanup_l(pj.pos, pk.pos, rjk_mag, rjk_mu);
+	        xijk=cf->xi(rjk_mag, rjk_mu);
+
+//			Checking if ik bin is in chosen range and assuming that "bin" is the correct ij bin
+			if ((binik<nbin*mbin) && (binik>=0)){
+				c3l=pi.w*pi.w*pj.w*pk.w/p3 * xijk;
+				c3[bin*nbin*mbin+binik]+=c3l;
+//				c3err[bin*nbin*mbin+binik]+=pow(c3l,2);
+				c3err[bin*nbin*mbin+binik]+=pi.w*pi.w*pj.w*pk.w/p3 * 0.5*(xijk*xiik+xijk*xiij); //Misuse to accumulate xi^2 term
+				binct3[bin*nbin*mbin+binik]++;
+			}
+
+#ifdef ALLOUT
+			rjk_magt=rjk_mag;
+	        rjk_mut=rjk_mu;
+#endif
+		}
+
+	    inline void fourth(const int& bin, Float& xijk, const Particle& pi, const Particle& pj, const Particle& pk, const Particle& pl, Float p4) {
+	    	// Accumulates the four-point integral C4
+	    	// Input: The ij bin, the correlation function between jk, the four particle positions
+	    	int binkl;
+	    	Float c4l,xiil;
+	    	Float ril_mag,ril_mu, rkl_mag,rkl_mu;
+
+	    	cleanup_l(pk.pos, pl.pos, rkl_mag, rkl_mu);
+	    	binkl=getbin(rkl_mag,rkl_mu);
+
+//			Checking if kl bin is in chosen range and assuming that "bin" is the correct ij bin
+	    	if ((binkl<nbin*mbin) && (binkl>=0)){
+
+		    	cleanup_l(pi.pos, pl.pos, ril_mag, ril_mu);
+		    	xiil=cf->xi(ril_mag, ril_mu);
+
+	    		c4l=pi.w*pj.w*pk.w*pl.w/p4  * xijk  * xiil;
+	    		c4[bin*nbin*mbin+binkl]+=c4l;
+//	    		c4err[bin*nbin*mbin+binkl]+=pow(c4l,2);
+	    		c4err[bin*nbin*mbin+binkl]+=pi.w*pj.w*pk.w*pl.w/p4; //Misuse of c4err to test normalization against 2pt randoms
+	    		binct4[bin*nbin*mbin+binkl]++;
+	    	}
+
+#ifdef ALLOUT
+#pragma omp critical
+	    		fprintf(rij_magf[bin],"%d %e %e %e %e %e %e %e %e %e %e\n",binkl,rij_mut,rkl_mu,rjk_mut,ril_mu,rij_magt,rkl_mag,rjk_magt,ril_mag, pi.w*pj.w*pk.w*pl.w/p4 * xijk  * xiil, pi.w*pj.w*pk.w*pl.w/p4);
+#endif
+	    }
+
+	    void sum_ints(Integrals* ints) {
+	    	// Add the values accumulated in ints to the corresponding internal sums
+			for(int i=0;i<nbin*mbin;i++){
+				Ra[i]+=ints->Ra[i];
+				c2[i]+=ints->c2[i];
+				cx[i]+=ints->cx[i];
+				binct[i]+=ints->binct[i];
+				Raerr[i]+=ints->Raerr[i];
+				c2err[i]+=ints->c2err[i];
+				cxerr[i]+=ints->cxerr[i];
+			}
+
+			for(int i=0;i<nbin*mbin;i++){
+				for(int j=0;j<nbin*mbin;j++){
+					c3[i*nbin*mbin+j]+=ints->c3[i*nbin*mbin+j];
+					c4[i*nbin*mbin+j]+=ints->c4[i*nbin*mbin+j];
+					c3err[i*nbin*mbin+j]+=ints->c3err[i*nbin*mbin+j];
+					c4err[i*nbin*mbin+j]+=ints->c4err[i*nbin*mbin+j];
+					binct3[i*nbin*mbin+j]+=ints->binct3[i*nbin*mbin+j];
+					binct4[i*nbin*mbin+j]+=ints->binct4[i*nbin*mbin+j];
+				}
+			}
+		}
+
+	    void sum_total_counts(uint64& acc2, uint64& acc3, uint64& acc4){
+	    	// Add local counts to total bin counts in acc2-4
+//	    	acc2=0,acc3=0,acc4=0;
+	    	for (int i=0; i<nbin*mbin; i++) {
+	    		acc2+=binct[i];
+				for (int j=0; j<nbin*mbin; j++) {
+					acc3+=binct3[i*nbin*mbin+j];
+					acc4+=binct4[i*nbin*mbin+j];
+				}
+			}
+	    }
+
+	    void normalize(long n,int np,int ngal, int n3, int n4) {
+	    	// Normalize the acccumulated integrals; partially already done by normalization of the proposal function of the cubes selected
+	    	// np is the number of random particles used, ngal is the number of galaxies in the survey
+	    	double corrf=pow((double)np/ngal,2); // Correction factor for the different densities of randoms and points
+
+	    	for(int i=0;i<nbin*mbin;i++){
+
+//	    		printf("Predefined Integrals %2d %le +- %le   %le +- %le   %le +- %le\n",
+//						i, Ra[i], Raerr[i], cx[i], cxerr[i], c2[i], c2err[i]);
+
+	    		Ra[i]/=((Float)n*corrf);
+	    		c2[i]/=(Ra[i]*Ra[i]*(Float)n*corrf);
+				cx[i]/=(Ra[i]*(Float)n*corrf);
+
+// TODO: Figure out how to normalize the quadratic counts of err to arrive at an estimate for the integration error
+//				Raerr[i]=sqrt((Raerr[i]/((Float)n)-pow(Ra[i]*corrf,2))/(Float)(n-1))/corrf;
+///*wrong->*/		c2err[i]=sqrt((c2err[i]/(pow((Ra[i]*Ra[i]*corrf*corrf),2)*(Float)n)-pow(c2[i]/corrf,2))/(Float)(n-1))/corrf;
+///*wrong->*/		cxerr[i]=sqrt((cxerr[i]/(pow((Ra[i]*pow((double)np/ngal,2)),2)*(Float)n)-pow(cx[i],2))/(Float)(n-1));
+
+//				printf("Postdefined Integrals %2d %le +- %le   %le +- %le   %le +- %le\n",
+//						i, Ra[i], Raerr[i], cx[i], cxerr[i], c2[i], c2err[i]);
+
+
+	    	}
+
+	    	for(int i=0;i<nbin*mbin;i++){
+	    		for(int j=0;j<nbin*mbin;j++){
+	    			c3[i*nbin*mbin+j]/=(n3*Ra[i]*Ra[j]*(Float)n*pow((double)np/ngal,3));
+	    			c4[i*nbin*mbin+j]/=(n3*n4*Ra[i]*Ra[j]*(Float)n*pow((double)np/ngal,4));
+	    		}
+			}
+	    }
+
+	    void report_integrals() {
+	///*
+		for (int j=0; j<nbin*mbin; j++) {
+		    printf("C2 %2d %le +- %le   %le +- %le   %le +- %le\n",
+				j, Ra[j], Raerr[j], cx[j], cxerr[j], 2*c2[j], 2*c2err[j]);
+		}
+		printf("\n");
+
+
+		for(int i=0;i<nbin*mbin;i++){
+			printf("C3 %2d ",i);
+			for(int j=0;j<nbin*mbin;j++){
+				printf("%e ", 4*c3[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for(int i=0;i<nbin*mbin;i++){
+			printf("C4 %2d ",i);
+			for(int j=0;j<nbin*mbin;j++){
+				printf("%e ",2*c4[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for(int i=0;i<nbin*mbin;i++){
+			printf("E3 %2d ",i);
+			for(int j=0;j<nbin*mbin;j++){
+				printf("%e ",4*c3err[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for(int i=0;i<nbin*mbin;i++){
+			printf("E4 %2d ",i);
+			for(int j=0;j<nbin*mbin;j++){
+				printf("%e ",2*c4err[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for (int i=0; i<nbin; i++) {
+			printf("N2 %2d ",i);
+			for (int j=0; j<mbin; j++) {
+				printf("%lld ", binct[i*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for (int i=0; i<nbin*mbin; i++) {
+			printf("N3 %2d ",i);
+			for (int j=0; j<nbin*mbin; j++) {
+				printf("%lld ", binct3[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		for (int i=0; i<nbin*mbin; i++) {
+			printf("N4 %2d ",i);
+			for (int j=0; j<nbin*mbin; j++) {
+				printf("%lld ", binct4[i*nbin*mbin+j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
+		fflush(NULL);
+
+	//*/
+	/*
+		printf("\n{");
+		printf("%.0f", xi0[0]);
+		for (int j=1; j<nbin; j++) {
+		    printf(",%.0f", xi0[j]);
+		}
+		printf("}\n");
+	*/
+
+	    }
+
+	  private:
+	    void cleanup_l(Float3 p1,Float3 p2,Float& norm,Float& mu){
+	    	Float3 pos=p1-p2;
+	    	norm = pos.norm();
+	    	// If the input correlation function had only radial information and no mu bins
+	    	// fill the dummy mu bins by 0.5
+	    	if(rad){
+	    		mu=0.5;
+	    	}
+	    	else{
+#ifndef PERIODIC
+				Float3 los=p1+p2; // No 1/2 as normalized anyway below
+	//	    	if(!box){
+					//Absolute value as mu bins defined in terms of |mu|
+					mu = fabs(pos.dot(los)/norm/los.norm());
+	//	    	}
+#else
+	    		// In the periodic case use z-direction for mu
+				mu = fabs(pos.z/norm);
+#endif
+	    	}
+	    }
+	};
+#endif
---
+++ modules/parameters.h
@@ -0,0 +1,190 @@
+// parameter function file for grid_covariance.cpp (originally from Alex Wiegand)
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+class Parameters{
+
+public:
+	// Important variables to set!  Here are the defaults:
+	// The periodicity of the position-space cube.
+	Float boxsize = 400;
+
+	// The particles will be read from the unit cube, but then scaled by boxsize.
+	Float rescale = 0.0;   // If left zero or negative, set rescale=boxsize
+
+	// The maximum radius of the largest bin.
+	Float rmax = 200.0;
+
+	// The minimum radius of the smallest bin.
+	Float rmin = 0.0;
+
+	// The maximum mu of the largest bin.
+	Float mumax = 1.0;
+
+	// The minimum mu of the smallest bin.
+	Float mumin = 0.0;
+
+	// The radius beyond which the correlation function is set to zero
+	Float xicutoff = 1000.0;
+
+	Float nofznorm=626798;//6684485//681013//672940//674847 629310
+
+	// The grid size, which should be tuned to match boxsize and rmax.
+	// Don't forget to adjust this if changing boxsize!
+	int nside = 8;
+
+	// If set, we'll just throw random periodic points instead of reading the file
+	int make_random = 0;
+
+	// Will be number of particles in a random distribution,
+	// but gets overwritten if reading from a file.
+	int np = -1;
+
+	// Whether to balance the weights or multiply them by -1
+	int qbalance = 0, qinvert = 0;
+
+	// The maximum number of points to read
+	uint64 nmax = 1000000000000;
+
+    // The number of radial bins
+	int nbin = 25;
+
+    // The number of mu bins
+	int mbin = 1;
+
+	// The number of threads to run on
+	int nthread=10;
+
+	// The location and name of a integrated grid of probabilities to be saved
+	char *savename = NULL;
+	// The location and name of a integrated grid of probabilities to be loaded
+	char *loadname = NULL; //	"ProbListGrid100box10-corfu.dat"
+
+	// The index from which on to invert the sign of the weights
+	int rstart=0;
+
+	// Whether or not we are using a periodic box
+	bool perbox=false;
+
+	// The name of the input file
+	char *fname = NULL;
+	const char default_fname[500] = "sample.dat";
+
+	// The name of the correlation function file
+	char *corname = NULL;
+	const char default_corname[500] = "../grid_multipoles_own/PatchySkyCorrSingle361.xi";
+	//"../grid_multipoles_own/PatchySkyCorrSingle361.xi"//PatchySkyCorrMean.xi//QPMCorrMean.xi//QPMExtrapolated.xi//"QPM_D_ngc_rsd_fix3.xi"
+
+	// Constructor
+	Parameters(int argc, char *argv[]){
+
+	    if (argc==1) usage();
+	    int i=1;
+	    while (i<argc) {
+		     if (!strcmp(argv[i],"-boxsize")||!strcmp(argv[i],"-box")) boxsize = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-rescale")||!strcmp(argv[i],"-scale")) rescale = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-rmax")||!strcmp(argv[i],"-max")) rmax = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-rmin")) rmin = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-mumax")) mumax = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-mumin")) mumin = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-xicut")) xicutoff = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-norm")) nofznorm = atof(argv[++i]);
+		else if (!strcmp(argv[i],"-nside")||!strcmp(argv[i],"-ngrid")||!strcmp(argv[i],"-grid")) nside = atoi(argv[++i]);
+		else if (!strcmp(argv[i],"-in")) fname = argv[++i];
+		else if (!strcmp(argv[i],"-cor")) corname = argv[++i];
+		else if (!strcmp(argv[i],"-rs")) rstart = atoi(argv[++i]);
+		else if (!strcmp(argv[i],"-nmax")) nmax = atoll(argv[++i]);
+		else if (!strcmp(argv[i],"-nthread")||!strcmp(argv[i],"-nthreads")) nthread = atoi(argv[++i]);
+		else if (!strcmp(argv[i],"-nbin")) nbin = atoi(argv[++i]);
+		else if (!strcmp(argv[i],"-mbin")) mbin = atoi(argv[++i]);
+		else if (!strcmp(argv[i],"-save")||!strcmp(argv[i],"-store")) savename = argv[++i];
+		else if (!strcmp(argv[i],"-load")) loadname = argv[++i];
+		else if (!strcmp(argv[i],"-balance")) qbalance = 1;
+		else if (!strcmp(argv[i],"-invert")) qinvert = 1;
+		else if (!strcmp(argv[i],"-ran")||!strcmp(argv[i],"-np")) {
+			double tmp;
+			if (sscanf(argv[++i],"%lf", &tmp)!=1) {
+			    fprintf(stderr, "Failed to read number in %s %s\n",
+			    	argv[i-1], argv[i]);
+			    usage();
+			}
+			np = tmp;
+			make_random=1;
+		    }
+		else if (!strcmp(argv[i],"-def")||!strcmp(argv[i],"-default")) { fname = NULL; }
+		else {
+		    fprintf(stderr, "Don't recognize %s\n", argv[i]);
+		    usage();
+		}
+		i++;
+	    }
+	    assert(i==argc);  // For example, we might have omitted the last argument, causing disaster.
+
+	    assert(nside%2!=0); // The probability integrator needs an odd grid size
+
+	    assert(mumin>=0); // We take the absolte value of mu
+	    assert(mumax<=1); // mu > 1 makes no sense
+
+	    assert(boxsize>0.0);
+	    assert(rmax>0.0);
+	    assert(nside>0);
+	    if (rescale<=0.0) rescale = boxsize;   // This would allow a unit cube to fill the periodic volume
+	    if (fname==NULL) fname = (char *) default_fname;   // No name was given
+	    if (corname==NULL) { corname = (char *) default_corname; }//fprintf(stderr,"No correlation file."); return 1;}// No name was given
+
+#ifdef OPENMP
+		omp_set_num_threads(nthread);
+#else
+		nthread=1;
+#endif
+
+
+		// Output for posterity
+		printf("Box Size = %6.5e\n", boxsize);
+		printf("Grid = %d\n", nside);
+		printf("Maximum Radius = %6.5e\n", rmax);
+		Float gridsize = rmax/(boxsize/nside);
+		printf("Radius in Grid Units = %6.5e\n", gridsize);
+		if (gridsize<1) printf("#\n# WARNING: grid appears inefficiently coarse\n#\n");
+		printf("Radial Bins = %d\n", nbin);
+		printf("Radial Binning = {%6.5f, %6.5f, %6.5f}\n",rmin,rmax,(rmax-rmin)/nbin);
+		printf("Mu Bins = %d\n", mbin);
+		printf("Mu Binning = {%6.5f, %6.5f, %6.5f}\n",mumin,mumax,(mumax-mumin)/mbin);
+		printf("Density Normalization = %6.5e\n",nofznorm);
+
+	}
+private:
+	void usage() {
+	    fprintf(stderr, "\nUsage for grid_covariance:\n");
+	    fprintf(stderr, "   -box <boxsize> : The periodic size of the computational domain.  Default 400.\n");
+	    fprintf(stderr, "   -scale <rescale>: How much to dilate the input positions by.  Default 0.\n");
+	    fprintf(stderr, "             Zero or negative value causes =boxsize, rescaling unit cube to full periodicity\n");
+	    fprintf(stderr, "   -rmax <rmax>: The maximum radius of the largest pair bin.  Default 200.\n");
+	    fprintf(stderr, "   -xicut <xicutoff>: The radius beyond which xi is set to zero.  Default 1000.\n");
+	    fprintf(stderr, "   -nside <nside>: The grid size for accelerating the pair count.  Default 8.\n");
+	    fprintf(stderr, "             Recommend having several grid cells per rmax.\n");
+	    fprintf(stderr, "   -in <file>: The input file (space-separated x,y,z,w).  Default sample.dat.\n");
+	    fprintf(stderr, "   -ran <np>: Ignore any file and use np random perioidic points instead.\n");
+	    fprintf(stderr, "   -norm <nofznorm>: Number of galaxies in the survey. Used to normalize n(z).\n");
+	    fprintf(stderr, "   -def: This allows one to accept the defaults without giving other entries.\n");
+	    fprintf(stderr, "\n");
+        fprintf(stderr, "   -cor <file>: File location of inut correlation function file.\n");
+	    fprintf(stderr, "   -nbin:  The number of radial bins.\n");
+	    fprintf(stderr, "   -mbin:  The number of mu bins.\n");
+	    fprintf(stderr, "The radial bin spacing (currently linear) is hard-coded.\n");
+	    fprintf(stderr, "\n");
+	    fprintf(stderr, "For advanced use, there is an option store the grid of probabilities used for sampling.\n");
+	    fprintf(stderr, "    -save <filename>: Triggers option to store probability grid. <filename> has to end on \".bin\"\n");
+	    fprintf(stderr, "The file can then be reloaded on subsequent runs\n");
+	    fprintf(stderr, "    -load <filename>: Triggers option to load the probability grid\n");
+	    fprintf(stderr, "    -balance: Rescale the negative weights so that the total weight is zero.\n");
+	    fprintf(stderr, "    -invert: Multiply all the weights by -1.\n");
+
+
+	    exit(1);
+	}
+
+};
+
+#endif
