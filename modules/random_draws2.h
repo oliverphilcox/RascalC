d2be5557bac8abc2362b25a771c28d60aacd3ef8
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2018-11-23 22:20:12 -0500

    new k picking

--- modules/random_draws2.h
+++ modules/random_draws2.h
@@ -384,7 +384,7 @@ public:
             fval[0]= pow(x[0],2)/ (2*pow(R,3))*exp(-pow(x[0]/(2*R),2))*corr->xi(x[0]);//(abs(corr->xi(x[0]))+pow(r_min,2.)/pow(x[0]+r_min,4.)) ;
         } else{
             // Use full expression for non-zero n
-            fval[0] = x[0]*/ (R*n) * (exp(-factor_1)+exp(-factor_2))*corr->xi(x[0]);//(abs(corr->xi(x[0]))+pow(r_min,2.)/pow(x[0]+r_min,4.)) ;
+            fval[0] = x[0]/ (R*n) * (exp(-factor_1)+exp(-factor_2))*corr->xi(x[0]);//(abs(corr->xi(x[0]))+pow(r_min,2.)/pow(x[0]+r_min,4.)) ;
         }
         
         return 0;
