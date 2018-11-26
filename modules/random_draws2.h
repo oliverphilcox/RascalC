490fb45b15e444664c47a1a5a0dbc12469cac9f5
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2018-11-25 19:50:40 -0500

    corrected probability integrals

--- modules/random_draws2.h
+++ modules/random_draws2.h
@@ -384,10 +384,10 @@ public:
         
         if(n<=0){
             // Replace expression by Taylor series in this limit
-            fval[0]= pow(x[0],2)/ (pow(R,3))*exp(-pow(x[0]/(2*R),2))*(abs(corr->xi(x[0]))+25./pow(x[0]+r_min,2.)) ;
+            fval[0]= pow(x[0],2)/ (pow(R,3))*exp(-pow(x[0]/(2*R),2))*(abs(corr->xi(x[0]))+100./pow(x[0]+r_min,2.)) ;
         } else{ 
             // Use full expression for non-zero n
-            fval[0] = x[0]/ (R*n) * (exp(-factor_2)-exp(-factor_1))*(abs(corr->xi(x[0]))+25./pow(x[0]+r_min,2.)) ;
+            fval[0] = x[0]/ (R*n) * (exp(-factor_2)-exp(-factor_1))*(abs(corr->xi(x[0]))+100./pow(x[0]+r_min,2.)) ;
         }
         
         return 0;
