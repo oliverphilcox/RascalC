23c712040298766a4d0e98da3b2ddcadf187ae71
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2018-11-26 22:03:27 -0500

    debugging

--- modules/parameters.h
+++ modules/parameters.h
@@ -98,7 +98,7 @@ public:
     Float * radial_bins_high;
     
     char *out_file = NULL;
-    const char default_out_file[500] = "/mnt/store1/oliverphilcox/Tester/";
+    const char default_out_file[500] = "/mnt/store1/oliverphilcox/Gosling_jk_il_cube_cube/";
     
 	// Constructor
 	Parameters(int argc, char *argv[]){
