2de84045be12d62ef6084e26b4c1b17d89a45fb5
Oliver Philcox <ohep2@cam.ac.uk>
2018-12-18 09:47:47 -0500

    QPM Covariance Matrices

--- python/QPM_covariances.py
+++ python/QPM_covariances.py
@@ -11,31 +11,34 @@ cwd = "/home/oliverphilcox/COMAJE/"
 subprocess.run(["bash","clean"],cwd=cwd)
 subprocess.run(["make"],cwd=cwd)
 
-all_N_gal = np.zeros(99)
 
 # First compute all N_gal values:
-for index in range(99):
-    root_dir_m = '/mnt/store1/oliverphilcox/CMU/QPM for Oliver/QPM_Pairs_Mariana/'
-    mfile_norms = root_dir_m+'qpm-unrecon-%s-norm.dat'%str(index+1).zfill(4)
-    norms_M = np.loadtxt(mfile_norms,usecols=1)
-    factor = norms_M[0]/norms_M[1]
+if False:
+    all_N_gal = np.zeros(99)
     
-    # Define parameters
-    all_N_gal[index] =factor*6500000; 
-    
-# Now run code for the mean of the mocks
-N_gal = np.mean(all_N_gal)
-xi_file = "/mnt/store1/oliverphilcox/CMU/xi_functions_QPM/QPM_Mariana_mean.xi"
-outfile = "/mnt/store1/oliverphilcox/CMU/QPM_Covariances/Mean/"
-    
-print("RUNNING FOR MEAN XI \n\n")
-subprocess.run(["./cov","-norm", "%.8f"%N_gal,"-cor",xi_file,"-output",outfile],cwd=cwd)
+    for index in range(99):
+        root_dir_m = '/mnt/store1/oliverphilcox/CMU/QPM for Oliver/QPM_Pairs_Mariana/'
+        mfile_norms = root_dir_m+'qpm-unrecon-%s-norm.dat'%str(index+1).zfill(4)
+        norms_M = np.loadtxt(mfile_norms,usecols=1)
+        factor = norms_M[0]/norms_M[1]
+        
+        # Define parameters
+        all_N_gal[index] =factor*6500000; 
+        
+    # Now run code for the mean of the mocks
+    N_gal = np.mean(all_N_gal)
+    xi_file = "/mnt/store1/oliverphilcox/CMU/xi_functions_QPM/QPM_Mariana_mean.xi"
+    outfile = "/mnt/store1/oliverphilcox/CMU/QPM_Covariances/Mean/"
+        
+    print("RUNNING FOR MEAN XI \n\n")
+    subprocess.run(["./cov","-norm", "%.8f"%N_gal,"-cor",xi_file,"-output",outfile],cwd=cwd)
 
-this_time = time.time()-init
-print("\n\nMean of mocks finished after %d seconds (%.2f minutes = %.2f hours)\n\n"%(this_time,this_time/60.,this_time/3600.))
+    this_time = time.time()-init
+    print("\n\nMean of mocks finished after %d seconds (%.2f minutes = %.2f hours)\n\n"%(this_time,this_time/60.,this_time/3600.))
 
-max_QPM = 20
-for index in range(max_QPM):
+min_QPM = 20
+max_QPM = 50
+for index in range(min_QPM,max_QPM+1):
     # Iterate over QPM index
     root_dir_m = '/mnt/store1/oliverphilcox/CMU/QPM for Oliver/QPM_Pairs_Mariana/'
     mfile_norms = root_dir_m+'qpm-unrecon-%s-norm.dat'%str(index+1).zfill(4)
