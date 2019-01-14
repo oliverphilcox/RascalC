133b00d3dc7f40c4c6f55587c13e4d78e3c74d63
Oliver Philcox <ohep2@cam.ac.uk>
2019-01-14 10:16:59 -0500

    virtualenv

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='RascalC_utils',
-      version='1.0',
-      description='Approximate Covariance Matrices for Large Scale Structure Correlation Functions',
+      version='0.1',
+      description='Approximate Covariance Matrices for Large-Scale-Structure correlation functions',
+      url='http://github.com/oliverphilcox/RascalC',
       author="Oliver Philcox, Ross O'Connell, Daniel Eisenstein, Alex Wiegand",
       author_email='ohep2@alumni.cam.ac.uk',
-      url='https://rascalc.readthedocs.io/',
+      license='MIT',
       packages=['utils'],
-     )
-
+      zip_safe=False)
