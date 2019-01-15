bf047fd35706aae8d74da0a7d001f8707e155643
Oliver Philcox <ohep2@alumni.cam.ac.uk>
2019-01-15 12:41:43 +0000

    restructure

--- grid_covariance.cpp
+++ grid_covariance.cpp
@@ -1,4 +1,4 @@
-// grid_covariance.cpp -- Alexander Wiegand, started Oct 14, 2016 based on grid_multipoles.cpp of Daniel Eisenstein.
+// grid_covariance.cpp -- Oliver Philcox / Alexander Wiegand, started Oct 14, 2016 based on grid_multipoles.cpp of Daniel Eisenstein.
 
 #include <sys/time.h>
 #include <sys/resource.h>
@@ -7,7 +7,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
-//#include <random>
 #include <complex>
 #include <algorithm>
 #include <gsl/gsl_interp.h>
@@ -22,12 +21,8 @@
 #include <limits>
 #include <sys/stat.h>
 
-
-//#define OPENMP
-
 // For multi-threading:
 #ifdef OPENMP
-//#define _GNU_SOURCE
 #include <omp.h>
 #include <sched.h>
 #endif
@@ -42,7 +37,6 @@
 typedef unsigned long long int uint64;
 
 // Could swap between single and double precision here.
-// Only double precision has been tested.
 typedef double Float;
 typedef double3 Float3;
 
@@ -52,13 +46,13 @@ typedef double3 Float3;
 #include "modules/cell_utilities.h"
 #include "modules/grid.h"
 #include "modules/correlation_function.h"
-#include "modules/random_draws2.h"
+#include "modules/random_draws.h"
 #include "modules/integrals.h"
 #include "modules/driver.h"
 #include "modules/jackknife_weights.h"
 
-// Very ugly way to get the correlation function into the integrator, but hey, it works
-CorrelationFunction * RandomDraws2::corr;
+// Get the correlation function into the integrator
+CorrelationFunction * RandomDraws::corr;
 
 
 STimer TotalTime;
@@ -66,9 +60,7 @@ STimer TotalTime;
 // ====================  Computing the integrals ==================
 
 
-#include "modules/compute_integral3.h"
-
-
+#include "modules/compute_integral.h"
 
 // ================================ main() =============================
 
@@ -89,10 +81,8 @@ int main(int argc, char *argv[]) {
 
     
     //TODO: Remove true
-    if(true){ 
-        JK_weights tmp(&par,1,1);
-        all_weights[0].copy(&tmp); // save into global memory
-    }
+    JK_weights tmp(&par,1,1);
+    all_weights[0].copy(&tmp); // save into global memory
     
     if(par.multi_tracers==true){
         JK_weights tmp12(&par,1,2), tmp2(&par,2,2);
@@ -104,7 +94,6 @@ int main(int argc, char *argv[]) {
     Grid all_grid[no_fields]; // create empty grids
     
     for(int index=0;index<no_fields;index++){
-        //Grid this_grid;
         Float3 shift;
         Particle *orig_p;
         if (!par.make_random){
@@ -117,7 +106,6 @@ int main(int argc, char *argv[]) {
         } else {
         // If you want to just make random particles instead:
         assert(par.np>0);
-        //assert(par.boxsize==par.rescale);    // Nonsense if not!
         orig_p = make_particles(par.rect_boxsize, par.np);
         // set as periodic if we make the random particles
         par.perbox = true;
@@ -132,7 +120,7 @@ int main(int argc, char *argv[]) {
         if(index==1) nofznorm=par.nofznorm2;
         Grid tmp_grid(orig_p, par.np, par.rect_boxsize, par.nside, shift, nofznorm);
 
-        Float grid_density = (double)par.np/tmp_grid.nf;//pow(par.nside,3);
+        Float grid_density = (double)par.np/tmp_grid.nf;
         printf("\n RANDOM CATALOG %d DIAGNOSTICS:\n",index+1);
         printf("Average number of particles per grid cell = %6.2f\n", grid_density);
         Float max_density = 16.0;
@@ -169,32 +157,30 @@ int main(int argc, char *argv[]) {
     
     // Now define all possible correlation functions and random draws:
     CorrelationFunction all_cf[max_no_functions];
-    RandomDraws2 all_rd[max_no_functions];
-    //TODO: Remove true
-    if(true){
-        CorrelationFunction tmp_cf(par.corname,par.mbin,par.mumax-par.mumin);
-        all_cf[0].copy_function(&tmp_cf);
-        RandomDraws2 tmp_rd(&tmp_cf,&par,NULL,0);
-        all_rd[0].copy(&tmp_rd);//RandomDraws2(&cf,&par,NULL,0);
-    }
+    RandomDraws all_rd[max_no_functions];
+    
+    CorrelationFunction tmp_cf(par.corname,par.mbin,par.mumax-par.mumin);
+    all_cf[0].copy_function(&tmp_cf);
+    RandomDraws tmp_rd(&tmp_cf,&par,NULL,0);
+    all_rd[0].copy(&tmp_rd);
     
     if(par.multi_tracers==true){
         CorrelationFunction tmp_cf12(par.corname12,par.mbin,par.mumax-par.mumin), tmp_cf2(par.corname2,par.mbin,par.mumax-par.mumin);
         all_cf[1].copy_function(&tmp_cf2);
         all_cf[2].copy_function(&tmp_cf12);
-        RandomDraws2 rd2(&tmp_cf2,&par,NULL,0), rd12(&tmp_cf12,&par,NULL,0);
+        RandomDraws rd2(&tmp_cf2,&par,NULL,0), rd12(&tmp_cf12,&par,NULL,0);
         all_rd[1].copy(&rd2);
         all_rd[2].copy(&rd12);
     }
     
-    compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,1,1,1,1,1); // final digit is iteration number
+    compute_integral(all_grid,&par,all_weights,all_cf,all_rd,1,1,1,1,1); // final digit is iteration number
 
     if(par.multi_tracers==true){
-        compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,1,2,1,1,2);
-        compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,1,2,2,1,3);
-        compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,1,1,2,2,4);
-        compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,2,1,2,2,5);
-        compute_integral3(all_grid,&par,all_weights,all_cf,all_rd,2,2,2,2,6);
+        compute_integral(all_grid,&par,all_weights,all_cf,all_rd,1,2,1,1,2);
+        compute_integral(all_grid,&par,all_weights,all_cf,all_rd,1,2,2,1,3);
+        compute_integral(all_grid,&par,all_weights,all_cf,all_rd,1,1,2,2,4);
+        compute_integral(all_grid,&par,all_weights,all_cf,all_rd,2,1,2,2,5);
+        compute_integral(all_grid,&par,all_weights,all_cf,all_rd,2,2,2,2,6);
     }
     rusage ru;
     getrusage(RUSAGE_SELF, &ru);
