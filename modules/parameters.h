fb567ef6de1b875545492ec20749338fe1e8a1f7
Oliver Philcox <ohep2@cam.ac.uk>
2019-02-04 16:12:04 -0500

    update

--- modules/parameters.h
+++ modules/parameters.h
@@ -91,8 +91,8 @@ public:
     
     // Number of random cells to draw at each stage
     int N2 = 20; // number of j cells per i cell
-    int N3 = 30; // number of k cells per j cell
-    int N4 = 40; // number of l cells per k cell
+    int N3 = 40; // number of k cells per j cell
+    int N4 = 80; // number of l cells per k cell
     
     //-------- OTHER PARAMETERS ----------------------------------------------
     
