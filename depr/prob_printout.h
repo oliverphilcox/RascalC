520eee13720529d75ad6564bf373ad42e7a8fbc2
Oliver Philcox <ohep2@cam.ac.uk>
2019-02-09 15:54:28 -0500

    double counting correction in corrfunc

---
+++ depr/prob_printout.h
@@ -0,0 +1,14 @@
+ printf("REMOVE THIS");
+//                         delta2 = rd13->random_cubedraw(locrng,&p2);
+//                         // Printout p2 and delta2
+//                         cell_sep2 = grid2->cell_sep(delta2);
+//                         Float oneD_sep = sqrt(pow(cell_sep2[0],2.)+pow(cell_sep2[1],2.)+pow(cell_sep2[2],2.));
+//         
+//                         char counts_file[1000];
+//                         snprintf(counts_file, sizeof counts_file, "tmp_prob_cube.txt");
+//                         FILE * CountsFile = fopen(counts_file,"a");
+//                         fprintf(CountsFile,"%.8e %.8e\n",oneD_sep,p2);
+//                         fclose(CountsFile);
+//                         continue;   
+//                         
+                        
