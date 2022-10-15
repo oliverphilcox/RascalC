455f2e8c2ad6f058ac93346c5582d9be3f6b62cf
Michael Rashkovetskyi <misharash@gmail.com>
2022-10-15 15:05:08 -0400

    More options for extra convergence check script

--- python/convergence_check_extra.py
+++ python/convergence_check_extra.py
@@ -6,8 +6,8 @@ import numpy as np
 import sys,os
 
 # PARAMETERS
-if len(sys.argv) != 2:
-    print("Usage: python convergence_check_extra.py {RASCALC_RESULTS_FILE}")
+if len(sys.argv) not in (2, 3):
+    print("Usage: python convergence_check_extra.py {RASCALC_RESULTS_FILE} [{N_SUBSAMPLES_TO_USE}]")
     sys.exit()
 
 rascalc_results = str(sys.argv[1])
@@ -26,9 +26,17 @@ def KL_div_covs(C1, C2):
 
 with np.load(rascalc_results) as f:
     c_samples = f["individual_theory_covariances"]
+n_samples = int(sys.argv[2]) if len(sys.argv) >= 3 else len(c_samples)
 
-n_samples_2 = len(c_samples) // 2
+print("First splitting")
+n_samples_2 = n_samples // 2
 cov_first = np.mean(c_samples[:n_samples_2], axis=0)
-cov_second = np.mean(c_samples[n_samples_2:], axis=0)
+cov_second = np.mean(c_samples[n_samples_2:n_samples], axis=0)
+print("RMS eigenvalues of inverse tests for cov half-estimates are %.2e and %.2e" % (rms_eig_inv_test_covs(cov_first, cov_second), rms_eig_inv_test_covs(cov_second, cov_first)))
+print("KL divergences between cov half-estimates are %.2e and %.2e" % (KL_div_covs(cov_first, cov_second), KL_div_covs(cov_second, cov_first)))
+
+print("Second splitting")
+cov_first = np.mean(c_samples[:n_samples:2], axis=0)
+cov_second = np.mean(c_samples[1:n_samples:2], axis=0)
 print("RMS eigenvalues of inverse tests for cov half-estimates are %.2e and %.2e" % (rms_eig_inv_test_covs(cov_first, cov_second), rms_eig_inv_test_covs(cov_second, cov_first)))
 print("KL divergences between cov half-estimates are %.2e and %.2e" % (KL_div_covs(cov_first, cov_second), KL_div_covs(cov_second, cov_first)))
