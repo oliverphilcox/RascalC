4b5651bdf5508d20ffc09a19faaa48834f229208
Michael Rashkovetskyi <misharash@gmail.com>
2024-02-23 11:16:04 -0800

    Allowed already wrapped estimators in pycorr reshaping

--- RascalC/pycorr_utils/utils.py
+++ RascalC/pycorr_utils/utils.py
@@ -20,16 +20,9 @@ def fix_bad_bins_pycorr(xi_estimator: pycorr.twopoint_estimator.BaseTwoPointEsti
 
 
 def reshape_pycorr(xi_estimator: pycorr.twopoint_estimator.BaseTwoPointEstimator, n_mu: int | None = None, r_step: float | None = None, r_max: float = np.inf, skip_r_bins: int = 0) -> pycorr.twopoint_estimator.BaseTwoPointEstimator:
-    n_mu_orig = xi_estimator.shape[1]
-    if n_mu_orig % 2 != 0: raise ValueError("Wrapping not possible")
-    if n_mu:
-        if n_mu_orig % (2 * n_mu) != 0: raise ValueError("Angular rebinning not possible")
-        mu_factor = n_mu_orig // 2 // n_mu
-    else: mu_factor = 1 # leave the original number of mu bins
-
+    # determine the radius step in pycorr
     if not r_step: r_factor = 1
     else:
-        # determine the radius step in pycorr
         r_steps_orig = np.diff(xi_estimator.edges[0])
         r_step_orig = np.mean(r_steps_orig)
         if not np.allclose(r_steps_orig, r_step_orig, rtol=5e-3, atol=5e-3): raise ValueError("Radial rebinning only supported for linear bins")
@@ -37,9 +30,28 @@ def reshape_pycorr(xi_estimator: pycorr.twopoint_estimator.BaseTwoPointEstimator
         r_factor = int(np.rint(r_factor_exact))
         if not np.allclose(r_factor, r_factor_exact, rtol=5e-3): raise ValueError(f"Radial rebinning seems impossible: exact ratio of steps is {r_factor_exact}, closest integer is {r_factor} and that is too far")
 
+    # Skip the first bins as requested
+    xi_estimator = xi_estimator[skip_r_bins * r_factor:]
+
     # Apply r_max cut
     r_values = xi_estimator.sepavg(axis = 0)
     r_bins_indices = np.where(r_values <= r_max)[0]
     xi_estimator = xi_estimator[:r_bins_indices.max() + 1] # can not apply mask to pycorr estimators; the bins are adjacent anyway
 
-    return fix_bad_bins_pycorr(xi_estimator[skip_r_bins * r_factor:])[::r_factor, ::mu_factor].wrap() # first skip bins, then fix bad bins, then rebin and wrap to positive mu
+    # determine the mu binning factor
+    n_mu_orig = xi_estimator.shape[1]
+    need_wrap = xi_estimator.edges[1][0] < 0 # wrap if extends to negative mu
+    if need_wrap:
+        if n_mu_orig % 2 != 0: raise ValueError("Wrapping not possible")
+        n_mu_orig //= 2
+    if n_mu:
+        if n_mu_orig % n_mu != 0: raise ValueError("Angular rebinning not possible")
+        mu_factor = n_mu_orig // n_mu
+    else: mu_factor = 1 # leave the original number of mu bins
+
+    if need_wrap: xi_estimator = fix_bad_bins_pycorr(xi_estimator) # try to fix bad bins using symmetry
+    xi_estimator = xi_estimator[::r_factor, ::mu_factor] # rebin
+    if need_wrap: xi_estimator = xi_estimator.wrap() # wrap after rebinning, otherwise the results used to be weird
+    elif r_factor != 1 or mu_factor != 1: warn("Seems like the estimator has been wrapped before, nontrivial rebinning after wrapping might be troublesome")
+
+    return xi_estimator
