f3a8438089912d4c8b188118bb9eae099a6a121a
Michael Rashkovetskyi <misharash@gmail.com>
2024-04-30 00:06:11 -0400

    Ensured single-threaded jackknife assignment
    Because multi-threading in it is known to interfere with RascalC

--- RascalC/pre_process/create_jackknives_pycorr.py
+++ RascalC/pre_process/create_jackknives_pycorr.py
@@ -1,13 +1,21 @@
 """ This function will assign a jackknife region to each input particle, in a way compatible with pycorr used in DESI data processing. Data is saved as a 5 column text file."""
 
 import numpy as np
+import os
 import pycorr
 
 
 def get_subsampler_xirunpc(ref_positions: np.ndarray[float], njack: int) -> pycorr.KMeansSubsampler:
     if len(ref_positions) != 3: ref_positions = ref_positions.T
     if len(ref_positions) != 3: raise ValueError("The reference positions do not appear 3-dimensional")
-    return pycorr.KMeansSubsampler('angular', positions = ref_positions, position_type = 'rdd', nsamples = njack, nside = 512, random_state = 42)
+    # Remove the OMP_ environment variables temporarily
+    omp_keys = [key for key in os.environ if key.startswith("OMP_")]
+    omp_backup = {key: os.environ.pop(key) for key in omp_keys}
+    # Create the subsampler. The above steps disable multi-threading, which is known to interfere with RascalC C++ code. Later usage of the subsampler should remain single-threaded as we want it. The jackknife assignment takes very little time compared to the pair counting and especially RascalC importance sampling.
+    subsampler = pycorr.KMeansSubsampler('angular', positions = ref_positions, position_type = 'rdd', nsamples = njack, nside = 512, random_state = 42)
+    # Restore the OMP_ environment variables removed above
+    for key in omp_keys: os.environ[key] = omp_backup[key]
+    return subsampler
 
 
 def add_subsample_labels(subsampler: pycorr.twopoint_jackknife.BaseSubsampler, pos_etc: np.ndarray[float], position_type: str = "xyz") -> np.ndarray[float]:
